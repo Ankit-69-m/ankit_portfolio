cced1c4dab09d5a9f6865248d8b0d5a40004e318
Ankit Mourya <mouryaankit091@gmail.com>
2024-08-28 09:20:04 +0530

    first commit

---
+++ data/index.ts
@@ -0,0 +1,141 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Experience", link: "#experience" },
+  { name: "Approach", link: "#approach" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b12.png",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "Tools & Techniques",
+    description: "My Designing",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Proven Track Record of Delivering Exceptional Designs.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently Designing Products for Himalayan Threads",
+    description: "The Uttrakhand Brand",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-100 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b121.png",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to Hire me as a Graphic Designer?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Himaluxe Product Desiging",
+    des: "I Designing almost 20+ Product mockup for the Himaluxe following there theme and royality of himalayas.",
+    img: "/hima.jpg",
+    iconLists: ["/ps.png", "/ill.png", "/pack.jpg", "/can.png"],
+    link: "https://himaluxe.com/",
+  },
+  {
+    id: 2,
+    title: "Senseacademy UI/UX Design",
+    des: "Senseacademy is a child company of senselearner where they starting edtech and I along with team Designed the UI/UX Design",
+    img: "/sense.png.png",
+    iconLists: ["/word.png", "/fig.png", "ps.png", "can.png", "ill.png"],
+    link: "https://senseacademy.co/",
+  },
+  {
+    id: 3,
+    title: "Aanchal Diary Work",
+    des: "Redesigned Some of their Products and Designed Social Media Post Designs",
+    img: "/aanch.png",
+    iconLists: ["/ps.png", "/ill.png", "/pack.jpg", "/can.png"],
+    link: "https://drive.google.com/drive/folders/1B--ICAnhU0szwGugJ3yrM6VV7VPEn4FY?usp=sharing",
+  },
+  {
+    id: 4,
+    title: "Youtube Thumbnails ",
+    des: "Designed Youtube Thumbnails for Dynamo Gaming, Tvf, Payal Gaming & Mortal",
+    img: "/dyno.png.png",
+    iconLists: ["/ps.png", "/ill.png"],
+    link: "https://drive.google.com/drive/folders/1B--ICAnhU0szwGugJ3yrM6VV7VPEn4FY?usp=sharing",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Senior Graphic Designer",
+    desc: "At Senselearner --- Produced high-impact advertising banners, flyers, course PDFs, and mockups, aligning designs with cybersecurity and marketing trends & with company demands. Designed engaging websites and web applications, incorporating user-centered design principles and responsive layouts.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Web Developer & Designer",
+    desc: "At HackersForYou --- Developed and implemented robust web applications using ReactJs, NodeJs, Frontend Language.Designed engaging websites and web applications, incorporating user-centered design principles. I worked with cross-functional teams to design user-centric websites for our clients.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/Ankit-69-m",
+  },
+  {
+    id: 2,
+    img: "/insta.svg",
+    link: "https://www.instagram.com/ankit_69m/",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/ankit-mourya-b71ab4216/",
+  },
+];
